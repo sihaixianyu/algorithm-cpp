b33831a1af5b578c06a7ee87445e45bc8a87c3e3
sihaixianyu <sihaixianyu@gmail.com>
2023-03-26 20:01:10 +0800

    2023-03-26+

--- src/structure/linked_list.cpp
+++ src/structure/linked_list.cpp
@@ -1,49 +1,76 @@
-#include <assert.h>
-#include <stdlib.h>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
 
-typedef struct ListNode {
+using namespace std;
+
+struct ListNode {
     int val;
-    struct ListNode* next;
-} ListNode;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ~ListNode() {}
+};
 
-typedef struct LinkedList {
+class LinkedList {
+public:
     ListNode* head;
-} LinkedList;
 
-ListNode* new_list_node(const int val) {
-    ListNode* node = (ListNode*)malloc(sizeof(ListNode));
-    node->val = val;
-    node->next = NULL;
+    LinkedList() { head = nullptr; }
 
-    return node;
-}
+    ~LinkedList() {
+        auto curr = this->head;
 
-LinkedList init_linked_list(const int* arr, const int len) {
-    LinkedList list;
-
-    if (len == 0) {
-        return list;
+        while (curr != nullptr) {
+            auto prev = curr;
+            curr = curr->next;
+            // Release the space of node.
+            delete prev;
+        }
     }
 
-    ListNode* head = (ListNode*)malloc(sizeof(ListNode));
-    head->val = arr[0];
+    LinkedList(const vector<int>& nums) {
+        if (nums.size() == 0) {
+            return;
+        }
 
-    ListNode* curr = head;
-    for (int i = 1; i < len; i++) {
-        curr->next = (ListNode*)malloc(sizeof(ListNode));
+        auto head = new ListNode(nums[0]);
+        auto curr = head;
+
+        for (int i = 1; i < nums.size(); i++) {
+            auto node = new ListNode(nums[i]);
+            curr->next = node;
+            curr = curr->next;
+        }
+
+        this->head = head;
+    }
+
+    vector<int> traverse() { return LinkedList::traverse(this->head); }
+
+    static vector<int> traverse(ListNode* head);
+};
+
+vector<int> LinkedList::traverse(ListNode* head) {
+    auto nums = vector<int>();
+    auto curr = head;
+
+    while (curr != nullptr) {
+        nums.push_back(curr->val);
         curr = curr->next;
     }
 
-    list.head = head;
-
-    return list;
-}
-
-void test_init_binary_tree() {
+    return nums;
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    vector<int> nums = {1, 2, 3, 4, 5};
+    auto list = new LinkedList(nums);
 
-    LinkedList list = init_linked_list(arr, sizeof(arr) / sizeof(arr[0]));
+    auto ans = list->traverse();
+    assert(ans == nums);
+
+    delete list;
 }
